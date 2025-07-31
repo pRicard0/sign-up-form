99ec1b15319c37f4084d58108ac3a3b8073f94de
pRicard0 <pauloricardo.jesus2011@gmail.com>
2025-07-31 14:16:21 -0300

    Initial configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'cadastro', component: Register },
+    { path: 'home', component: Home },
+    { path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
